31492bdf59f3076c1aa17ee4241ffe3691b82d00
sancho1903 <sancho1903@gmail.com>
2016-10-01 16:09:48 +0300

    devam

---
+++ WebAppMVCBlog/Models/ORM/Entity/AdminUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVCBlog.Models.ORM.Entity
+{
+    public class AdminUser
+    {
+    }
+}
---
+++ WebAppMVCBlog/Models/ORM/Entity/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVCBlog.Models.ORM.Entity
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+        private DateTime _addDate = DateTime.Now;
+        public DateTime AddDate
+        {
+            get { return _addDate; }
+            set { _addDate = value; }
+        }
+        private bool _isDeleted = false;
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; }
+        }
+        public DateTime DeletedDate { get; set; }
+
+    }
+}
